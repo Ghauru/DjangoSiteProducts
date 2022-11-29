382f4bd257468ae620fc8d3363a92cf804432296
Ghauru <rulesox@gmail.com>
2022-11-29 15:21:03 +0300

    Models created

--- mysite/comp/models.py
+++ mysite/comp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    exist = models.BooleanField(default=False)
+    specifies = models.TextField()
+    reviews = models.TextField()
+    additional_information = models.TextField()
+    link = models.CharField(max_length=255)
+    image_link = models.CharField(max_length=255)
+    seller = models.CharField(100)
+
+
+class Market(models.Model):
+    name = models.CharField(max_length=50)
+    link = models.CharField(max_length=255)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    search_history = models.TextField()
