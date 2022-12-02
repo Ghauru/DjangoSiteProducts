96962b4ae7d64e6a248dd0c5d290729c59dcc3ff
Ghauru <rulesox@gmail.com>
2022-12-02 21:55:15 +0300

    index html/css about help updated

--- mysite/comp/views.py
+++ mysite/comp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'comp/index.html')
+
+
+def about(request):
+    return render(request, 'comp/about.html')
+
+
+def site_help(request):
+    return render(request, 'comp/help.html')
