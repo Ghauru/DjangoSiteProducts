29a6ca1ca3bcd65bbdcf68cd6b4793efc3853a44
Ghauru <rulesox@gmail.com>
2022-12-04 07:31:36 +0300

    Product test in card Card added Правки позиции текста

--- mysite/comp/views.py
+++ mysite/comp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+from django.http import Http404
 
 
 def index(request):
-    return render(request, 'comp/index.html')
-
+    try:
+        p = Product.objects.get()
+    except Product.DoesNotExist:
+        raise Http404("Товар не найден")
+    return render(request, 'comp/index.html', {'product': p})
 
 def about(request):
     return render(request, 'comp/about.html')
